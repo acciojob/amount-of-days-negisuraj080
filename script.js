af85929581843b86fa9091f366b0467438c0c643
negisuraj080 <143009358+negisuraj080@users.noreply.github.com>
2023-12-09 17:43:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function daysOfYear(year){
+	if(year%4 === 0){
+		if(year % 100 === 0){
+			return year % 400 === 0 ? 366 :365;
+		}
+		return 365;
+	}
+	return 365;
+}
